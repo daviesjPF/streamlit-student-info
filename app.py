af915eaf5eb60bcae5bac3ffb8a81bd653dac2c0
daviesjPF <jennifer.davies@pierre-fabre.com>
2024-05-31 11:59:55 +1000

    changed text of inputs

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 # Title of the app
 st.title("Student Information") 
 
-name = st.text_input("Name", "Tintin")
-age = st.slider("How old are you?", 1, 100, 24) #min, max, default
+name = st.text_input("Your Name", "Tintin")
+age = st.slider("Your Age?", 1, 100, 24) #min, max, default
 if st.button('Display Information'):
     f"Students name: {name}"
     f"Students age: {age}"
